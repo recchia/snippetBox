b5b6eccb0c71628e8e64a54540f9c1ea233a97af
Piero Recchia <piero.recchia@gmail.com>
2026-01-28 01:35:37 -0300

    Refactored and add tests

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,14 +6,34 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/recchia/snippetbox/pkg/forms"
-	"github.com/recchia/snippetbox/pkg/models"
+	"github.com/recchia/snippetbox/internal/models"
+	"github.com/recchia/snippetbox/internal/validator"
 )
+
+type snippetCreateForm struct {
+	Title               string `form:"title"`
+	Content             string `form:"content"`
+	Expires             int    `form:"expires"`
+	validator.Validator `form:"-"`
+}
+
+type userSignupForm struct {
+	Name                string `form:"name"`
+	Email               string `form:"email"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
+}
+
+type userSignInForm struct {
+	Email               string `form:"email"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
+}
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	s, err := app.snippets.Latest()
 	if err != nil {
-		app.serverError(w, err)
+		app.serverError(w, r, err)
 		return
 	}
 
@@ -36,7 +56,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		app.serverError(w, err)
+		app.serverError(w, r, err)
 		return
 	}
 
@@ -56,18 +76,19 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	form := forms.New(r.PostForm)
-	form.Required("title", "content", "expires")
-	form.MaxLength("title", 100)
-	form.PermittedValues("expires", "365", "7", "1")
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxLength(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
 	if !form.Valid() {
-		app.render(w, r, "create.page.html", &templateData{Form: form})
+		app.render(w, r, http.StatusUnprocessableEntity, "create.html", &templateData{Form: form})
+		return
 	}
 
-	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
-		app.serverError(w, err)
+		app.serverError(w, r, err)
 		return
 	}
 
@@ -80,32 +101,33 @@ func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	var form userSignupForm
 
+	err := app.decodePostForm(r, &form)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	form := forms.New(r.PostForm)
-	form.Required("name", "email", "password")
-	form.MaxLength("name", 255)
-	form.MaxLength("email", 255)
-	form.MatchesPattern("email", forms.EmailRX)
-	form.MinLength("password", 10)
+	form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+	form.CheckField(validator.MatchesPattern(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+	form.CheckField(validator.MinLength(form.Password, 10), "password", "This field must be at least 10 characters long")
 
 	if !form.Valid() {
-		app.render(w, r, "signup.page.html", &templateData{Form: form})
+		app.render(w, r, http.StatusUnprocessableEntity, "signup.html", &templateData{Form: form})
 		return
 	}
 
-	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+	err = app.users.Insert(form.Name, form.Email, form.Password)
 
 	if err != nil {
 		if errors.Is(err, models.ErrDuplicateEmail) {
-			form.Errors.Add("email", "Email is already in use")
-			app.render(w, r, "signup.page.html", &templateData{Form: form})
+			form.AddFieldError("email", "Email address is already in use")
+			app.render(w, r, http.StatusUnprocessableEntity, "signup.html", &templateData{Form: form})
 		} else {
-			app.serverError(w, err)
+			app.serverError(w, r, err)
 		}
 
 		return
@@ -128,15 +150,23 @@ func (app *application) userSignInPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	form := forms.New(r.PostForm)
-	id, err := app.users.Authenticate(form.Get("email"), form.Get("password"))
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.MatchesPattern(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+
+	if !form.Valid() {
+		app.render(w, r, http.StatusUnprocessableEntity, "signin.html", &templateData{Form: form})
+		return
+	}
+
+	id, err := app.users.Authenticate(form.Email, form.Password)
 
 	if err != nil {
 		if errors.Is(err, models.ErrInvalidCredentials) {
 			form.Errors.Add("generic", "Email or Password is incorrect")
 			app.render(w, r, "signin.page.html", &templateData{Form: form})
 		} else {
-			app.serverError(w, err)
+			app.serverError(w, r, err)
 		}
 
 		return
